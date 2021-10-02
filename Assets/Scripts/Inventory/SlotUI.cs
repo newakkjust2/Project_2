26df7bed29f3bba918b37f1ea1af15c2543bf47f
ironlionm4n <65094447+ironlionm4n@users.noreply.github.com>
2021-10-02 11:26:18 -0400

    Updated Fast Inventory
    BUG: When you use the items all of a type of item the sprite does not change back to default empty slot ... sometimes lol

--- Assets/Scripts/Inventory/SlotUI.cs
+++ Assets/Scripts/Inventory/SlotUI.cs
@@ -18,23 +18,19 @@ public class SlotUI : MonoBehaviour
             _defaultSprite = icon.sprite;
     }
 
-    public void UpdateUI(int itemQuantity, string itemName, AllVariantsItemSO collection)
+    public void RefreshSlotUI(int itemQuantity, string itemName, AllVariantsItemSO collection)
     {
         int itemKey = collection.keys.IndexOf(itemName);
-        ItemSO temp = collection.values[itemKey];   
-        if(icon != null)
-            icon.sprite = temp.Icon;
-        else
-        {
-            Debug.Log("Icon in collection is null");
-        }
+        ItemSO temp = collection.values[itemKey];
+        icon.sprite = temp.Icon;
+        gameObject.name = itemName;
         quantity.text = itemQuantity.ToString();
-        
     }
 
-    public void UpdateUI()
+    public void RefreshSlotUI()
     {
         icon.sprite = _defaultSprite;
         quantity.text = String.Empty;
+        gameObject.name = "Slot Empty";
     }
 }
